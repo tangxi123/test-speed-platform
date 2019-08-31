ee52bfcb51d81f8c6e0bafd47c392c1f3858c3c1
Tang xi <tangxi669@163.com>
2019-08-31 13:26:26 +0800

    webHook测试

--- src/main/java/org/tangxi/testplatform/service/LogService.java
+++ src/main/java/org/tangxi/testplatform/service/LogService.java
@@ -52,7 +52,7 @@ public class LogService {
             List<TestCaseLog> testCaseLogs = logMapper.getTestCaseLogsByFields(params);
             PageInfo<TestCaseLog> logs = new PageInfo<>(testCaseLogs);
             LOG.info("查询到的测试用例列表为：{}", testCaseLogs);
-            LOG.info("返回的分页列表：{}", logs);
+            LOG.info("返回的分页列表为：{}", logs);
             return new Response<>(200, logs, "查询成功");
         } catch (Exception e) {
             throw new UnexpectedTestCaseException(e);
